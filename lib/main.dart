cee8dd28f8792ba932af06275804f59e9fa64127
teewhydope <ibrahimanimasahun@gmail.com>
2024-11-08 11:59:09 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:zojatech_assessment/core/app/app.dart';
 import 'package:zojatech_assessment/core/di/injector.dart';
 
 void main() {
   configureDependencies();
-  runApp(const App());
+  runApp(const ProviderScope(child: App()));
 }
